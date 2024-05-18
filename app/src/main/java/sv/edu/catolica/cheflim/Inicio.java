f0e283970c0a69bd7c1a8318496bc9003842eef7
xaldakr <50474978+xaldakr@users.noreply.github.com>
2024-05-17 21:03:04 -0600

    activities

---
+++ app/src/main/java/sv/edu/catolica/cheflim/Inicio.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.cheflim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
