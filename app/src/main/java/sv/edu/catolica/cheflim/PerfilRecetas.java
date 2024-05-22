d9e49d15ea7116118bac31f8c04e5d7b62272a6d
xaldakr <50474978+xaldakr@users.noreply.github.com>
2024-05-22 00:47:38 -0600

    cardos

--- app/src/main/java/sv/edu/catolica/cheflim/PerfilRecetas.java
+++ app/src/main/java/sv/edu/catolica/cheflim/PerfilRecetas.java
@@ -2,7 +2,9 @@ package sv.edu.catolica.cheflim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PerfilRecetas extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class PerfilRecetas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_recetas);
+    }
+
+
+    public void IngresarListadelPerReceta(View view) {
+        Intent intento = new Intent(this, Listaingredientes.class);
+        startActivity(intento);
+        finish();
+    }
+    public void IngresarIniciodelPerReceta(View view) {
+        Intent intento = new Intent(this, Inicio.class);
+        startActivity(intento);
+        finish();
+    }
+    public void IngresarPerfildelPerReceta(View view) {
+        Intent intento = new Intent(this, PerfilDatos.class);
+        startActivity(intento);
+        finish();
+    }
+    public void IngresarDescubredelPerReceta(View view) {
+        Intent intento = new Intent(this, Descubre.class);
+        startActivity(intento);
+        finish();
     }
 }
